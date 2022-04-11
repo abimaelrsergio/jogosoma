89bbd3328b3fdffe1e601106e91127d3e0b33815
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-04-11 19:47:38 -0300

    upadate

--- jogosoma/src/component/jogo.js
+++ jogosoma/src/component/jogo.js
@@ -4,17 +4,22 @@ import { View, Text, StyleSheet } from 'react-native';
 class Jogo extends React.Component {
     render() {
         return (
-            <View style={ estilo.container }>
-                <Text>567</Text>
+            <View style={ estilos.container }>
+                <Text style={estilos.target}>567</Text>
             </View>
         );
     }
 }
 
-const estilo = StyleSheet.create({
+const estilos = StyleSheet.create({
     container: {
         backgroundColor: '#fff',
         flex: 1,
+        paddingTop: 30,
+    },
+    target: {
+        fontSize: 40,
+        backgroundColor: '#aaa',
     }
 });
 
